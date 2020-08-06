9b2bbcfa99d6e317aebe82305bb626707a287ca5
Luis Zabala <lzabala28@gmail.com>
2020-08-05 23:37:17 -0400

    se agrega docs

---
+++ docs/precache-manifest.195db9d518ec7c8339553e646f597985.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b838af8514d5e8281d115a5eee3ff949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2ed0794f58731adf4ae",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7d4590646f2dbbeea60d",
+    "url": "/static/css/main.cf1cfdcf.chunk.css"
+  },
+  {
+    "revision": "a2ed0794f58731adf4ae",
+    "url": "/static/js/2.89a45c27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89a45c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4590646f2dbbeea60d",
+    "url": "/static/js/main.aedb9a5e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
